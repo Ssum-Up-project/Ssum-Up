6b8f671b92429657e61d634693ce862972ed2543
7ryean <u7ryean@7ryean.localdomain>
2022-02-24 16:09:56 +0900

    test preprocessing text

--- ai/bert-extractive-summarization/gy_usage.py
+++ ai/bert-extractive-summarization/gy_usage.py
@@ -8,7 +8,7 @@ checkpoint = torch.load(f'checkpoints/{model_type}_ext.pt', map_location='cpu')
 model = ExtSummarizer(checkpoint=checkpoint, bert_type=model_type, device='cpu')
 
 # Run summarization
-input_fp = 'raw_data/input.txt'
-result_fp = 'results/summary.txt'
+input_fp = 'raw_data/WSJ_GY.txt'
+result_fp = 'results/GY_N.txt'
 summary = summarize(input_fp, result_fp, model, max_length=3)
 print(summary)
