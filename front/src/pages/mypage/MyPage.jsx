c33deaeb72f52f7d3255c235d03d78fb81cecfb5
world970511 <57740138+world970511@users.noreply.github.com>
2022-03-09 01:08:06 +0900

    test:my page 수정

--- front/src/pages/mypage/MyPage.jsx
+++ front/src/pages/mypage/MyPage.jsx
@@ -1,26 +1,75 @@
 import React,{useEffect, useState} from 'react';
+import {
+    Grid,
+    Box,
+    InputLabel,
+    MenuItem,
+    FormControl,
+    Select,
+    Paper
+} from '@mui/material';
 import UserService from "../../service/user.service";
+import Playlist from "./PlaylistCarousel"
+import RecentVideos from "./RecentCarousel"
 
 const MyPageContent=()=>{
-    const [content, setContent] = useState("");
+    const [contents, setContents] = useState([]);
     useEffect(()=>{
         UserService.getPlayList().then(
             (response) => {
                 console.log(response.data)
-                setContent(response.data);
+                setContents(response.data);
             },(error) => {
                 console.log("error")
             }
           );
         }, []);
+    
+    
+    //카테고리별 플레이리스트
+    const savedPlaylist=contents.filter(
+        (arr, index, callback) => index === callback.findIndex(data => data.list_name === arr.list_name)
+      );
+    const [category, setCategory] = useState("");
+    const handleChange = (event) => {
+        setCategory(event.target.value);
+     };
+    const selectData = contents.filter(data => data.list_name===category);
+
     return(
-        <div>
-            {content && content.map(post => (
-            <div key = {post.id}>
-              <h1>{post.list_name}</h1>
-            </div>))}
-        </div>
+        <Box
+         sx={{
+          p: 5,
+          margin: "auto",
+        }}
+      >
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+            <FormControl variant="standard" sx={{minHeight:20, minWidth: 120}}>
+            <InputLabel id="demo-simple-select-label">Category</InputLabel>
+                <Select
+                    labelId="demo-simple-select-label"
+                    id="demo-simple-select"
+                    value={category}
+                    label="Category"
+                    onChange={handleChange}
+                >
+                {savedPlaylist.map((playlist,index) => (
+                    <MenuItem value={playlist.list_name} key={index}>{playlist.list_name}</MenuItem>))}
+                </Select>
+            </FormControl>
+        </Grid>
+        <Grid item xs={12}>        
+           {category?(<Playlist category={selectData}/>):(<Playlist category={savedPlaylist[0]}/>)}
+        </Grid>
+        <Grid item xs={12}>
+            <div>최근 기록</div>
+        </Grid>
+        <Grid item xs={12}>
+            <RecentVideos videos={contents}/>
+        </Grid>
+      </Grid>
+    </Box>
         );
 }
 export default MyPageContent;
-
