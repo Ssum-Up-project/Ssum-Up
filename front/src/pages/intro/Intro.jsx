aeeead26d51e7345126e0ac3a3069d34783b2416
[3기 - 2반] 이상지 <sangji9504@gmail.com>
2022-03-10 01:02:53 +0900

    feat: Intro 진행 중

--- front/src/pages/intro/Intro.jsx
+++ front/src/pages/intro/Intro.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
-import Footer from "../../components/Footer";
 import Home from "./Home";
 import WOW from "wowjs";
+import "./Intro.css";
 
 function Intro() {
   useEffect(() => {
@@ -9,10 +9,19 @@ function Intro() {
   }, []);
 
   return (
-    <>
-      <Home />
-      <Footer />
-    </>
+    <div className="Intro">
+      <div className="text_container">
+        <h1>SSum Up</h1>
+        <p>
+          Wall Street Journal의 시사뉴스부터 The Verge의 최신 테크 뉴스까지,
+          <br />
+          영어와 시간의 제약을 넘어 더욱 효율적으로 유튜브를 이용하는 방법
+        </p>
+        <button className="intro__btn">시작하기</button>
+      </div>
+
+      <div className="img__container"></div>
+    </div>
   );
 }
 
