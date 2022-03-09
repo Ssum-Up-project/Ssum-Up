0e16f528e6bc9f1df8cecd1a0876b12462cc3237
[3기 - 2반] 이상지 <sangji9504@gmail.com>
2022-03-10 01:04:38 +0900

    polish: Main CSS & GlobalStyle

--- front/src/styles/GlobalStyle.jsx
+++ front/src/styles/GlobalStyle.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import { createGlobalStyle, GlobalStyles } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
-* {
+body {
   background-color: #191718;
   color: #E8E1C2;
   border-radius: 3px;}
@@ -15,3 +15,4 @@ h1, h2, h3, div, p{
 `;
 
 export default GlobalStyle;
+//  red #E05448
