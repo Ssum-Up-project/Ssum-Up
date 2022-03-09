493700a06e3c498c3175cd30d021da116cf462ae
[3기 - 2반] 이상지 <sangji9504@gmail.com>
2022-03-10 01:03:51 +0900

    fix: Team페이지 개선 중

--- front/src/pages/team/Team.jsx
+++ front/src/pages/team/Team.jsx
@@ -1,28 +1,29 @@
 import React from "react";
 import "./Team.css";
 import CardItem from "./CardItem";
+import Introduction from "./Introduction";
 
 function Team() {
   return (
     <div className="cards">
-      <h1>팀페이지 샘플</h1>
+      <Introduction />
       <div className="cards__container">
         <div className="cards__wrapper">
           <ul className="cards__items">
             <CardItem
-              src="images/img-9.jpg"
+              src=""
               text="Lorem ipsum dolor sit amet, consectetur adipiscing elit,...."
               label="pic 0"
               path="/"
             />
             <CardItem
-              src="images/img-9.jpg"
+              src=""
               text="Lorem ipsum dolor sit amet, consectetur adipiscing elit,...."
               label="pic 1"
               path="/"
             />
             <CardItem
-              src="images/img-2.jpg"
+              src=""
               text="Lorem ipsum dolor sit amet, consectetur adipiscing elit,...."
               label="pic 2"
               path="/"
@@ -30,19 +31,19 @@ function Team() {
           </ul>
           <ul className="cards__items">
             <CardItem
-              src="images/img-3.jpg"
+              src=""
               text="Lorem ipsum dolor sit amet, consectetur adipiscing elit,...."
               label="pic 3"
               path="/"
             />
             <CardItem
-              src="images/img-4.jpg"
+              src=""
               text="Lorem ipsum dolor sit amet, consectetur adipiscing elit,...."
               label="pic 4"
               path="/products"
             />
             <CardItem
-              src="images/img-8.jpg"
+              src=""
               text="Lorem ipsum dolor sit amet, consectetur adipiscing elit,...."
               label="pic 5"
               path="/sign-up"
