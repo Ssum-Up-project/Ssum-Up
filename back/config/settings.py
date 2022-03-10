7c17a06bd155db2da86d3d5ef0728dda5904671e
ss.k <kos7114@naver.com>
2022-03-10 21:06:23 +0900

    fix:naming isue

--- back/config/settings.py
+++ back/config/settings.py
@@ -96,10 +96,21 @@ WSGI_APPLICATION = "config.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "myboard",
+        "USER": "root",
+        "PASSWORD": "akfqkfrnq6",
+        "HOST": "localhost",
+        "PORT": "",
     }
 }
 
