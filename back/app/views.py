2bd82a5357bef8c85f3aeb2cc047c87d391f6537
[3기-2반] 김승수 <kos7114@naver.com>
2022-03-11 05:04:31 +0000

    Fix mypage

--- back/app/views.py
+++ back/app/views.py
@@ -13,12 +13,12 @@ from rest_framework import status
 from rest_framework import permissions
 
 from .serializers import PlayListSerializer
-from .serializers import (
-    VideoDataListSerializer,
-    VideoDataPostSerializer,
-    VideoDataResponseSerializer,
-    )
+from .serializers import PlayListDetailSerializer
+from .serializers import VideoDataListSerializer
+from .serializers import VideoDataPostSerializer
+from .serializers import VideoDataResponseSerializer
 from .serializers import SearchLogSerializer
+from .serializers import SearchLogDetailSerializer
 from .serializers import UserSerializer
 from .serializers import PlayListPostSerializer
 from .exceptions import AlreadyVideoInPlaylist
@@ -40,7 +40,7 @@ class PlayLists(APIView):
 
     @swagger_auto_schema(
         responses={
-            status.HTTP_200_OK: PlayListSerializer,
+            status.HTTP_200_OK: PlayListDetailSerializer,
             status.HTTP_400_BAD_REQUEST: "잘못된 요청",
         }
     )
@@ -52,7 +52,7 @@ class PlayLists(APIView):
         """
         self.user = self.get_user()
         playlist = PlayList.objects.filter(user_id=self.user.id)
-        serializer = PlayListSerializer(playlist, many=True)
+        serializer = PlayListDetailSerializer(playlist, many=True)
         return Response(serializer.data)
 
     @swagger_auto_schema(
@@ -93,7 +93,7 @@ class PlayListDetail(APIView):
 
     @swagger_auto_schema(
         responses={
-            status.HTTP_200_OK: PlayListSerializer,
+            status.HTTP_200_OK: PlayListDetailSerializer,
             status.HTTP_400_BAD_REQUEST: "잘못된 요청",
         }
     )
@@ -104,10 +104,11 @@ class PlayListDetail(APIView):
         특정 playlist의 video_id들을 가져옵니다.
         '''
         playlist = self.get_object(pk)
-        serializer = PlayListSerializer(playlist)
+        serializer = PlayListDetailSerializer(playlist)
         return Response(serializer.data)
 
     @swagger_auto_schema(
+        request_body=PlayListSerializer,
         responses={
             status.HTTP_200_OK: PlayListSerializer,
             status.HTTP_400_BAD_REQUEST: "잘못된 요청",
@@ -184,7 +185,7 @@ class VideoDataList(APIView):
             # 로그인 확인
             if request.user.is_authenticated:
                 # save searchlog
-                searchlog_input = {"user_id": request.user.id, "video_id": video[0].id}
+                searchlog_input = {"user_id": request.user.id, "video_data_id": video[0].id}
                 serializer_searchlog = SearchLogSerializer(data=searchlog_input)
                 if serializer_searchlog.is_valid(raise_exception=True):
                     serializer_searchlog.save()
@@ -205,7 +206,7 @@ class VideoDataList(APIView):
             # 로그인 확인
             if request.user.is_authenticated:
                 # save searchlog
-                searchlog_input = {"user_id": request.user.id, "video_id": video_data.id}
+                searchlog_input = {"user_id": request.user.id, "video_data_id": video_data.id}
                 serializer_searchlog = SearchLogSerializer(data=searchlog_input)
                 if serializer_searchlog.is_valid(raise_exception=True):
                     serializer_searchlog.save()
@@ -260,7 +261,7 @@ class SearchLogList(APIView):
 
     @swagger_auto_schema(
         responses={
-            status.HTTP_200_OK: SearchLogSerializer,
+            status.HTTP_200_OK: SearchLogDetailSerializer,
             status.HTTP_400_BAD_REQUEST: "잘못된 요청",
         }
     )
@@ -274,7 +275,7 @@ class SearchLogList(APIView):
             searchlog = SearchLog.objects.all()[:10]
         except SearchLog.DoesNotExist:
             searchlog = None
-        serializer = SearchLogSerializer(searchlog, many=True)
+        serializer = SearchLogDetailSerializer(searchlog, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
 
@@ -282,7 +283,7 @@ class SearchLogUserList(APIView):
 
     @swagger_auto_schema(
         responses={
-            status.HTTP_200_OK: SearchLogSerializer,
+            status.HTTP_200_OK: SearchLogDetailSerializer,
             status.HTTP_400_BAD_REQUEST: "잘못된 요청",
         }
     )
@@ -296,5 +297,5 @@ class SearchLogUserList(APIView):
             searchlog = SearchLog.objects.filter(user_id=request.user.id)
         except SearchLog.DoesNotExist:
             searchlog = None
-        serializer = SearchLogSerializer(searchlog, many=True)
+        serializer = SearchLogDetailSerializer(searchlog, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
